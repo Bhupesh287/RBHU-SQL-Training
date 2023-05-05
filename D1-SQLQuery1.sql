68d96f521907fef09e76ccc1664514b6ebbe3a2a
Bhupesh287 <120743433+Bhupesh287@users.noreply.github.com>
2023-05-05 11:58:14 +0530

    Add files via upload

---
+++ D1-SQLQuery1.sql
@@ -0,0 +1,7 @@
+create database mytestdb
+use mytestdb
+create table mytesttable(rollno int, firstname varchar(50), lastname varchar(50))
+
+select * from mytesttable
+
+insert into mytesttable(rollno, firstname, lastname) values(1,'Bhupesh','Ghadigaonkar')
